913853b906b78018bdef3718575188b427341200
Andy Lamothe-Futu <2272637@collegemv.qc.ca>
2025-05-12 10:54:47 -0400

    ajout de Lundi

--- Projet.py
+++ Projet.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as pmc
-import socket
+from pigpio_dht import DHT11, DHT22
 import pigpio
 import time
 import busio
@@ -10,24 +10,21 @@ from adafruit_ads1x15.ads1115 import P0
 from adafruit_ads1x15.analog_in import AnalogIn
 from flask import Flask, jsonify, request
 import pigpio
-BROKER = "mqttbroker.lan"
+BROKER = "10.10.1.151"
 PORT = 1883
 BTN = 2
+SENSOR_TH = 4
+R = 27
+B = 17
+W = 22
 TOPIC_TEMPERATURE = "/final/andy@andy/T"
 TOPIC_HUMIDITY = "/final/andy@andy/H"
-
-
-# Initialisation de l'interface i2c
-i2c = busio.I2C(board.SCL, board.SDA)
- 
-# Créer une instance de la classe ADS1115 
-# et l'associer à l'interface i2c
-ads = ADS1115(i2c)
- 
-# Créer une instance d'entrée analogique
-# et l'associer à la broche 0 du module ADC
-data = AnalogIn(ads, P0)
- 
+app = Flask(__name__)
+pi = pigpio.pi()
+pi.set_mode(R,pigpio.INPUT)
+pi.set_mode(B,pigpio.INPUT)
+pi.set_mode(W,pigpio.INPUT)
+pi.set_mode(BTN,pigpio.INPUT)
 
 def connexion(client, userdata, flags, code, properties):
     if code == 0:
@@ -35,66 +32,49 @@ def connexion(client, userdata, flags, code, properties):
     else:
         print("Erreur code %d\n", code)
 
-def messageH():
-
-    moyenne = 0
-    while True:
-        
-        result1 = " la luminosité : "+ str(int(data.value * 100 / 26767)) + "% , Le voltage : " + str(int(data.voltage * 100 / 3.3)) + "%"
-        result =   int(data.voltage)
-        client.publish(TOPIC_HUMIDITY,result)
-        
-        print(data.value, data.voltage)
-        time.sleep(0.5)
-
-def messageT():
-
-    moyenne = 0
-    while True:
-        
-        result1 = " la luminosité : "+ str(int(data.value * 100 / 26767)) + "% , Le voltage : " + str(int(data.voltage * 100 / 3.3)) + "%"
-        result =   int(data.voltage)
-        client.publish(TOPIC_TEMPERATURE,result)
-        
-        print(data.value, data.voltage)
-        time.sleep(0.5)
 
 client = pmc.Client(pmc.CallbackAPIVersion.VERSION2)
 client.on_connect = connexion
-
-
 client.connect(BROKER,PORT)
-messageH()
-messageT()
 
-client.disconnect()
+try:
+    while True:
 
+        sensor = DHT11(SENSOR_TH)
+        result = sensor.read()
+        
+        print(result)
+        x = result["temp_c"]
+        y = result["humidity"]
+        print(x,y)
+        if x > y:
+            # Température supérieur
+            pi.write(R,1)
+            pi.write(B,0)
+            pi.write(W,0)
 
+        if y > x:
+            # Humidité supérieur
+            pi.write(R,0)
+            pi.write(B,1)
+            pi.write(W,0)
+        valeurH = str(y) + "%" 
+        valeurT = str(x) + "°C"
+        print(valeurH +" "+ valeurT)
+        client.publish(TOPIC_HUMIDITY,valeurH)
+        client.publish(TOPIC_TEMPERATURE,valeurT)
+        time.sleep(1)
+        valeur = 1
+        if pi.read(BTN) == 0:
+                valeur == 0
+                valLive = pi.read(BTN)
+                if valeur == 0 and valLive == 0:
+                    valeur != valeur
 
-app = Flask(__name__)
-pi = pigpio.pi()
-pi.set_mode(BTN,pigpio.INPUT)
+except KeyboardInterrupt:
+    client.disconnect()
+    pi.write(R,0)
+    pi.write(B,0)
+    pi.write(W,0)
+    pi.stop()
 
-valeur = 1
-if pi.read(BTN) == 0:
-        valeur == 0
-        valLive = pi.read(BTN)
-        if valeur == 0 and valLive == 0:
-            valeur != valeur
-            messageT()
-            messageH()
-
-@app.route('/donnees',methods=['GET'])
-def get_donnees():
-    d = {}
-    d['etat'] = pi.read(BTN)
-    messageH()
-    messageT()
-    time.sleep(30)
-
-    
-    
-    return jsonify(d)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=3000)
